d9d0e7dd8a94612f83f1cc457c0de454d0804be5
Leno rafael <73203800+lenors@users.noreply.github.com>
2022-06-10 15:02:01 -0300

    pesca

---
+++ app.js
@@ -0,0 +1,83 @@
+/* console.log(3 > 2) // > maior que
+console.log(3 < 2) // menor que  */
+
+//operadores de Comparação
+// = atribuição de tipo let nome = 'leandro'
+// == verificar o valor 
+// === verifica tipo e o valor
+// ! inverte os valores
+// != verifica se os valores comparados são diferentes
+// !== verifica se os valores e tipo comparados são diferentes
+// >= maior ou igual
+// <= menor ou igual
+// em valores binarios, 0 é false, e 1 true
+
+//Estrutura Condicional 
+//if e se
+
+/* if(condição){
+    console.log('retorno')
+} */
+
+//se a condição for verdadeira , executar tarefa
+
+/* let age = false
+
+if(age = Boolean ){
+   alert('ola, você está correto')
+} */
+
+//else se não
+
+/* if(condição){
+    console.log('condição é verdadeira')
+}else{
+    console.log('condição não é verdadeira')
+} */
+
+/* let joao = 16
+
+if(joao >= 18){
+    console.log('acesso permitido')
+}else{
+    console.log('acesso negado')
+} */
+
+/* let temLuz = false
+let isTrue = 'Acender as luzes'
+let isFalse = 'aguarde a energia voltar'
+
+if(temLuz === true){
+    console.log(isTrue)
+}else{
+    console.log(isFalse)
+} */
+
+//Operadores Logicos
+// && (and) seu resultado é true se ambos os valores forem verdadeiros
+// || (or) produz um valor true se qualquer um dos valores forem verdadeiros
+//!(not) inverto o valor que dado à ele
+
+/* console.log( true && false) //false
+console.log(true && true) //true
+console.log(false || true) //true
+console.log(false || false) //false */
+  
+/* let a = 3 > 2 //verdadeiro
+let b = 2 > 3 //falso
+
+    true
+if(a == true && b == false){
+    console.log('as duas condições são verdade')
+}else{
+    console.log('as duas condições são falsas')
+} */
+
+/*  let azul = 'azul'
+let vermelho = 'amarelo'
+
+if(azul == 'vermelho' || vermelho ==  'vermelho'){
+    console.log('verdade')
+}else{
+    console.log('mentira')
+} */
